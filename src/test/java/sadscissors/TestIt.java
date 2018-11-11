6f78ad1baba7e2ad38955a3121d5710940921f80
Emory Merryman <emory.merryman@gmail.com>
2018-11-11 14:53:41 +0000

    almost

--- src/test/java/sadscissors/TestIt.java
+++ src/test/java/sadscissors/TestIt.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static java.lang.Long.parseLong;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * WTF.
@@ -42,7 +43,7 @@ public final class TestIt {
     /**
      * Stereotypical Item Type.
      **/
-    private static final long TYPE = parseLong("-574284650089157818");
+    private static final long ITEM_TYPE = parseLong("-574284650089157818");
 
     /**
      * Stereotypical item uuid.
@@ -74,31 +75,7 @@ public final class TestIt {
      * Test the handleItemRemoved method by
      *
      * <OL>
-     *           <LI> Verifying that the item is not in the fridge.
-     *           <LI> Adding the item.
-     *           <LI> Verifying that the item is in the fridge.
-     *           <LI> Removing the item.
-     *           <LI> Verifying that the item is not in the fridge.
-     * </OL>
-     **/
-    @Test
-    public void testHandleItemRemovedPlus() {
-        assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
-        fridge.handleItemAdded(TYPE, ITEM_UUID, ITEM_NAME, HALF);
-        assertEquals(1, fridge.getItems(HALF).length);
-        assertEquals(HALF, fridge.getFillFactor(TYPE), EPSILON);
-        fridge.handleItemRemoved(ITEM_UUID);
-        assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
-    }
-
-    /**
-     * Test the handleItemRemoved method by
-     *
-     * <OL>
-     *           <LI> Verifying that the item is not in the fridge.
-     *           <LI> Adding the item.
+     *           <LI> Adding the item at HALF fill.
      *           <LI> Verifying that the item is in the fridge.
      *           <LI> Removing the item.
      *           <LI> Verifying that the item is not in the fridge.
@@ -106,14 +83,12 @@ public final class TestIt {
      **/
     @Test
     public void testHandleItemRemoved() {
-        assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
-        fridge.handleItemAdded(TYPE, ITEM_UUID, ITEM_NAME, HALF);
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, HALF);
         assertEquals(1, fridge.getItems(HALF).length);
-        assertEquals(HALF, fridge.getFillFactor(TYPE), EPSILON);
+        assertEquals(HALF, fridge.getFillFactor(ITEM_TYPE), EPSILON);
         fridge.handleItemRemoved(ITEM_UUID);
         assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
+        assertEquals(ZERO, fridge.getFillFactor(ITEM_TYPE), EPSILON);
     }
 
     /**
@@ -121,7 +96,6 @@ public final class TestIt {
      * threshold.
      *
      * <OL>
-     *           <LI> Verifying that the item is not in the fridge.
      *           <LI> Adding the item at HALF_PLUS fill.
      *           <LI> Verifying that getItems(HALF) does not return the item.
      *           <LI> Verify that the item is in the fridge.
@@ -129,18 +103,15 @@ public final class TestIt {
      **/
     @Test
     public void testGetItemsPlus() {
-        assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
-        fridge.handleItemAdded(TYPE, ITEM_UUID, ITEM_NAME, HALF_PLUS);
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, HALF_PLUS);
         assertEquals(0, fridge.getItems(HALF).length);
-        assertEquals(HALF_PLUS, fridge.getFillFactor(TYPE), EPSILON);
+        assertEquals(HALF_PLUS, fridge.getFillFactor(ITEM_TYPE), EPSILON);
     }
 
     /**
      * Test that getItems and getFillFactor will ignore empty containers.
      *
      * <OL>
-     *           <LI> Verifying that the item is not in the fridge.
      *           <LI> Adding and empty container (the item at ZERO fill).
      *           <LI> Adding the item at HALF fill.
      *           <LI> Verifying that getItems(HALF) does not consider the empty
@@ -150,11 +121,51 @@ public final class TestIt {
      * </OL>
      **/
     @Test
-    public void testGetItemsPlus() {
-        assertEquals(0, fridge.getItems(ZERO).length);
-        assertEquals(ZERO, fridge.getFillFactor(TYPE), EPSILON);
-        fridge.handleItemAdded(TYPE, ITEM_UUID, ITEM_NAME, HALF_PLUS);
+    public void testEmptyContainer() {
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, ZERO);
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, HALF);
+        assertEquals(1, fridge.getItems(HALF).length);
+        assertEquals(HALF, fridge.getFillFactor(ITEM_TYPE), EPSILON);
+    }
+
+    /**
+     * Test that getItems is returning in the array of arrays format.
+     *
+     * <OL>
+     *           <LI> Adding the item at HALF fill.
+     *           <LI> Verifying getItems
+     * </OL>
+     **/
+    @Test
+    public void testGetItems() {
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, HALF);
+        assertEquals(1, fridge.getItems(HALF).length);
+        assertTrue(fridge.getItems(HALF)[0].getClass().isArray());
+        Object[] item = (Object[]) fridge.getItems(HALF)[0];
+        assertEquals(2, item.length);
+        assertEquals(ITEM_TYPE, item[0]);
+        assertEquals(Double.class, item[1].getClass());
+        double fillFactor = (double) item[1];
+        assertEquals(HALF, fillFactor, EPSILON);
+    }
+
+    /**
+     * Test that forgetItems is forgetting.
+     *
+     * <OL>
+     *           <LI> Adding the item.
+     *           <LI> Verifying that the items got added.
+     *           <LI> Forgetting the item type.
+     *           <LI> Verifying that the item is forgotten.
+     * </OL>
+     **/
+    @Test
+    public void testForgetItem() {
+        fridge.handleItemAdded(ITEM_TYPE, ITEM_UUID, ITEM_NAME, HALF);
+        assertEquals(1, fridge.getItems(HALF).length);
+        assertEquals(HALF, fridge.getFillFactor(ITEM_TYPE), EPSILON);
+        fridge.forgetItem(ITEM_TYPE);
         assertEquals(0, fridge.getItems(HALF).length);
-        assertEquals(HALF_PLUS, fridge.getFillFactor(TYPE), EPSILON);
+        assertEquals(ZERO, fridge.getFillFactor(ITEM_TYPE), EPSILON);
     }
 }
